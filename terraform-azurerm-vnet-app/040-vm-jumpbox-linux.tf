11b02a6008dffd928038c9fd12b34c39ef5dc9e0
Roger Doherty <rdoherty@microsoft.com>
2023-12-01 00:33:06 -0500

    add jumplinux1 dependency to storage config

--- terraform-azurerm-vnet-app/040-vm-jumpbox-linux.tf
+++ terraform-azurerm-vnet-app/040-vm-jumpbox-linux.tf
@@ -31,13 +31,14 @@ resource "azurerm_linux_virtual_machine" "vm_jumpbox_linux" {
   admin_username        = data.azurerm_key_vault_secret.adminuser.value
   network_interface_ids = [azurerm_network_interface.vm_jumbox_linux_nic_01.id]
   patch_mode            = "AutomaticByPlatform"
-  tags                  = merge(
-                            var.tags, 
-                            { keyvault = var.key_vault_name }, 
-                            { adds_domain_name = var.adds_domain_name },
-                            { storage_account_name = var.storage_account_name },
-                            { storage_share_name = var.storage_share_name }
-                          )
+  depends_on            = [azurerm_virtual_machine_extension.vm_jumpbox_win_postdeploy_script]
+  tags = merge(
+    var.tags,
+    { keyvault = var.key_vault_name },
+    { adds_domain_name = var.adds_domain_name },
+    { storage_account_name = var.storage_account_name },
+    { storage_share_name = var.storage_share_name }
+  )
 
   admin_ssh_key {
     username   = data.azurerm_key_vault_secret.adminuser.value
