d30416eace24e5c92707fddb40e98b063ec3d738
Roger Doherty <rdoherty@microsoft.com>
2025-01-17 16:12:09 -0500

    chore: update terraform providers

--- terraform-azurerm-vnet-app/010-common.tf
+++ terraform-azurerm-vnet-app/010-common.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azapi = {
       source  = "azure/azapi"
-      version = "=2.1.0"
+      version = "=2.2.0"
     }
 
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.14.0"
+      version = "=4.16.0"
     }
 
     random = {
