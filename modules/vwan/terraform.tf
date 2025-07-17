1f0884040ddd7541fcd64bce28c4129b13a3747b
Roger Doherty <rdoherty@microsoft.com>
2025-07-17 11:59:49 -0400

    Implement network isolation for key vault Fixes #176

--- modules/vwan/terraform.tf
+++ modules/vwan/terraform.tf
@@ -2,6 +2,11 @@ terraform {
   required_version = "~> 1.12.2"
 
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.5.0"
+    }
+
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.35.0"
