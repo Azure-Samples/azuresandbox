f84ed65a038833efd1101cee00a552347865f8fe
Roger Doherty (MSFT) <rdoherty@microsoft.com>
2025-10-21 17:49:05 -0400

    chore: update terraform providers

--- extras/configurations/rg-devops-iac/terraform.tf
+++ extras/configurations/rg-devops-iac/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.13.3"
+  required_version = "~> 1.13.4"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.48.0"
+      version = "~> 4.49.0"
     }
 
     cloudinit = {
