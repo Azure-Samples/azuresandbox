7dd281d3ec622f4de38b6ad5eaa812e730014c4a
Roger Doherty (MSFT) <rdoherty@microsoft.com>
2026-04-16 17:34:29 -0400

    Network isolate devops key vault and storage account Fixes #312

--- extras/configurations/rg-devops-iac/network.tf
+++ extras/configurations/rg-devops-iac/network.tf
@@ -72,6 +72,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "key_vault" {
   resource_group_name   = azurerm_resource_group.this.name
   private_dns_zone_name = azurerm_private_dns_zone.key_vault.name
   virtual_network_id    = azurerm_virtual_network.this.id
+  resolution_policy     = "NxDomainRedirect"
 }
 
 
@@ -80,6 +81,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "storage_blob" {
   resource_group_name   = azurerm_resource_group.this.name
   private_dns_zone_name = azurerm_private_dns_zone.storage_blob.name
   virtual_network_id    = azurerm_virtual_network.this.id
+  resolution_policy     = "NxDomainRedirect"
 }
 #endregion
 
